41806ca3fbd55edcfedef4a15f765f07fa66251f
jenkincstang <66237680+jenkincstang@users.noreply.github.com>
2020-06-17 11:31:44 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+1.
+var user = {};
+user.name = "John";
+user.surname = "Mike";
+user.name = "Peter";
+delete user.name;
 
+console.log(user);
+
+2.
+var fruit = {
+	apple: 20,
+	pear: 20,
+	peach: 10
+};
+
+var totalFruit = fruit.apple + fruit.peach + fruit.pear;
+console.log(totalFruit);
